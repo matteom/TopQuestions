21a50d75ae32d04bc60a3cc0d34b91ea337992b2
Steve Graesser <s.graesser@computer.org>
2021-08-29 13:27:09 -0700

    Fixes matteom/TopQuestions#2. Adding date decoding strategy for correct reading of dates in sample data.

--- TopQuestions/Preview Content/TestData.swift
+++ TopQuestions/Preview Content/TestData.swift
@@ -11,7 +11,9 @@ struct TestData {
 	static var Questions: [Question] = {
 		let url = Bundle.main.url(forResource: "Questions", withExtension: "json")!
 		let data = try! Data(contentsOf: url)
-		let wrapper = try! JSONDecoder().decode(Wrapper<Question>.self, from: data)
+		let decoder = JSONDecoder()
+		decoder.dateDecodingStrategy = .secondsSince1970
+		let wrapper = try! decoder.decode(Wrapper<Question>.self, from: data)
 		return wrapper.items
 	}()
 	
